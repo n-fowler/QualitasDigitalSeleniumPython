ea1276920bf0f9bc0ac050b64872cdf9176361a0
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-02-15 05:12:09 -0500

    add remaining test data

---
+++ QualitasDigitalSeleniumPython/src/TestData/ContactUsPageTestData.py
@@ -0,0 +1,10 @@
+class ContactUsPageTestData(object):
+    faq_link = "https://www.qualitasdigital.com/faq"
+    header_content = "How Can We Help?"
+    left_content = "We want to hear from you.  Use the form on the right to request additional information about the services we offer."
+    our_commitment_link = "https://www.qualitasdigital.com/our-commitment"
+    terms_and_conditions_link = "https://www.qualitasdigital.com/terms-conditions"
+
+
+
+
---
+++ QualitasDigitalSeleniumPython/src/TestData/PricingAndServicesPageTestData.py
@@ -0,0 +1,29 @@
+class PricingAndServicesPageTestData(object):
+    additional_test_coverage_body_text = "$100/hour- We work within your existing workflow to add additional test coverage.  This is a simple no frills option that adds immediate value to your organization."
+    additional_test_coverage_title = "Additional Test Coverage"
+    coaching_body_text = "$100/hour- We meet with your team and identify areas of growth, pain points, and blockers.  We then work with your team to resolve these issues now and in the future."
+    coaching_title = "Coaching"
+    dashboards_disclaimer_body_text = "*The SaaS Dashboard product is not publicly available at this time.  Keep an eye out for the Beta announcement!"
+    dashboards_on_premise_body_text = "On Premise: $100/hour- We create a custom tailored dashboard that shows the business value and status of your tests.  Depending on the framework and language, we also extend your ‘out of the box’ solutions with additional functionality and value."
+    dashboards_saas_body_text = "Software as a Service*: variable cost per user- Sign up for our SaaS Dashboard and enjoy an easy setup and always on functionality at a low cost to you."
+    dashboards_title = "Dashboards"
+    framework_updates_custom_tailored_body_text = "Custom Tailored: $150/hour- We create a framework that meets your needs.  This often includes an extensive amount of system specific logging, as well as extensions and documentation that make the framework easy for your engineers to use compared with other frameworks in the industry."
+    framework_updates_title = "Framework Updates"
+    framework_updates_upgrades_body_text = "Upgrades/Replacement: $100/hour- Covers all upgrades and updates to existing frameworks such as updating automation to use functionality from a new version or replacing one framework with a different one.  We even perform language changes within existing frameworks such as moving automation from Selenium Java to Selenium C# without the loss of existing tests."
+    our_commitment_link = "https://www.qualitasdigital.com/our-commitment"
+    pricing_and_services_subtitle = "Software Quality Automation services starting at $100/hour."
+    pricing_and_services_title = "Pricing + Services"
+    process_automation_body_text = "$125/hour- We analyze your workflows and processes to identify ‘time wasters’ and repetitive tasks that make good candidates for automation.  We document and automate those processes so that your engineers can focus on the things that matter most to you."
+    process_automation_title = "Process Automation"
+    roadmaps_body_text = "$125/hour- We create a 1,3,5 year plan that accomplishes your business objectives.  These plans include multiple versions for Management, and Engineers.  Combined with our other services, this is an effective way to realize your business objectives and communicate them throughout the company."
+    roadmaps_title = "Road maps"
+    technical_cofounder_body_text = "$250/hour- Our founder helps you create your own startup.  This includes policies and procedures for attracting and hiring talent, the selection and use of initial automation frameworks that match your desired tech stack, training for your senior executives, and quality standards that allow you to maintain a competitive advantage."
+    technical_cofounder_title = "Technical Co-Founder"
+    training_body_text = "$100/hour- Personalized training that meets your needs.  Whether you need one on one training, group based training, or a combination of the two, we offer hands on labs and classroom style instruction that brings your engineers up to speed quickly."
+    training_title = "Training"
+    trusted_advisor_body_text = "$200/hour- Our Founder works alongside your senior executives to scale your software quality operations.  This covers how to attract talent, the technical interview process, and managing your teams."
+    trusted_advisor_title = "Trusted Advisor"
+
+
+
+
